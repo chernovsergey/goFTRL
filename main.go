929578a639159e6581f83cb2613ed4833193a077
chernovsergey <chernov.sergey23@gmail.com>
2019-05-27 23:07:37 +0300

    added flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"runtime/pprof"
@@ -9,27 +10,34 @@ import (
 	ml "github.com/go-code/ml/utils"
 )
 
+const (
+	pProf = "bench.pprof"
+)
+
 func main() {
-	//log.SetOutput(ioutil.Discard)
+	train := flag.String("-t", "./files/train_dataset.svm", "path to TRAIN data")
+	trainW := flag.String("-tw", "./files/weights_train.csv", "path to TRAIN weights file")
+	trainF := flag.String("-tf", "", "path to TRAIN feature names")
 
-	// TODO START
-	// Makte it as problem config
-	fileDir := "/Users/sergey/Downloads/"
+	valid := flag.String("-v", "./files/valid_dataset.svm", "path to VALID data")
+	validW := flag.String("-vw", "./files/weights_valid.csv", "path to VALID weights file")
+	validF := flag.String("-vf", "", "path to VALID feature names")
 
-	trainFile := fileDir + "train_dataset.svm"
-	trainWeights := fileDir + "weights_train.csv"
+	alpha := flag.Float64("-a", 0.15, "alpha")
+	beta := flag.Float64("-b", 1.0, "beta")
+	l1 := flag.Float64("-l1", 0.5, "L1")
+	l2 := flag.Float64("-l2", 1.0, "L2")
+	clip := flag.Float64("-clip", 1000.0, "gradient clip value")
+	tol := flag.Float64("-tol", 1e-4, "tolerance")
 
-	validFile := fileDir + "valid_dataset.svm"
-	validWeights := fileDir + "weights_valid.csv"
+	nEpoch := flag.Uint64("-e", 10, "number of epochs to train")
+	bench := flag.Bool("-pprof", true, "enable profiling")
 
-	//featureNames := fileDir + "feature_names.csv"
-	//TODO END
+	flag.Parse()
 
-	Dtrain := ml.MakeAndLoadDataset(trainFile, -1, true)
-	Dtrain.LoadSampleWeights(trainWeights)
-
-	Dvalid := ml.MakeAndLoadDataset(validFile, -1, true)
-	Dvalid.LoadSampleWeights(validWeights)
+	if *bench {
+		log.Println("pprof enabled!")
+	}
 
 	f, err := os.Create("bench.pprof")
 	if err != nil {
@@ -42,13 +50,38 @@ func main() {
 	}
 	defer pprof.StopCPUProfile()
 
+	// Parse train
+	Dtrain := ml.MakeAndLoadDataset(*train, -1, true)
+	if *trainW != "" {
+		Dtrain.LoadSampleWeights(*trainW)
+		if *validF != "" {
+			Dtrain.LoadFeatureNames(*trainF)
+		}
+	}
+
+	// Parse validation
+	var Dvalid *ml.Dataset
+	if *valid != "" {
+		Dvalid = ml.MakeAndLoadDataset(*valid, -1, true)
+		if *validW != "" {
+			Dvalid.LoadSampleWeights(*validW)
+		}
+		if *validF != "" {
+			Dvalid.LoadFeatureNames(*validF)
+		}
+	}
+
+	// Train model
 	params := ftrl.MakeParams(
-		0.15, 1.0, 0.1, 1.0,
-		1000, 0.0, 1e-4,
-		10, 'b')
+		*alpha, *beta, *l1, *l2,
+		*clip, 0.0, *tol,
+		*nEpoch, 'b')
+
 	logreg := ftrl.MakeFTRL(params)
 	logreg.Fit(Dtrain, Dvalid)
+
 	p := logreg.PredictBatch(Dvalid)
+
 	log.Println(ml.Mean(p))
 	logreg.DecisionSummary()
 }
