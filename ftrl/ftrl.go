5702899589add3cb9d50cc4cf937ce5cfc92e566
sergey <chernov.sergey23@gmail.com>
2019-07-03 00:26:24 +0300

    Code clean up

--- ftrl/ftrl.go
+++ ftrl/ftrl.go
@@ -1,6 +1,7 @@
 package ftrl
 
 import (
+	"fmt"
 	"log"
 	"math"
 	"os"
@@ -8,6 +9,35 @@ import (
 
 	util "github.com/go-code/goFTRL/utils"
 )
+
+type Params struct {
+	alpha, beta, lambda1, lambda2 float64
+	clipgrad                      float64
+	dropout                       float64
+	tol                           float64
+	niter                         uint64
+	activation                    rune
+}
+
+func MakeParams(
+	a, b, l1, l2, clipgrad, dropout, tol float64,
+	maxiter uint64, activation rune) Params {
+	return Params{
+		alpha:      a,
+		beta:       b,
+		lambda1:    l1,
+		lambda2:    l2,
+		clipgrad:   clipgrad,
+		dropout:    dropout,
+		tol:        tol,
+		niter:      maxiter,
+		activation: activation}
+}
+
+func (p *Params) String() string {
+	return fmt.Sprintf("FTRL{Alpha:%v, Beta:%v, L1:%v, L2:%v, max_iter:%v, activation:%q}",
+		p.alpha, p.beta, p.lambda1, p.lambda2, p.niter, p.activation)
+}
 
 // FTRL is a structure for "Follow The Regularized Leader"
 // logistic regression algorithm
@@ -41,17 +71,6 @@ func (a *FTRL) AllocWeightsStore(size uint64) {
 		panic("Ooops! Weights are already allocated")
 	}
 	a.weights = make([]*weights, size, size)
-}
-
-// FitStream fits model from stream
-func (a *FTRL) FitStream(stream DataStream, loss chan float64) {
-	for o := range stream {
-		x, y, sampleW := o.X, o.Y, o.W
-		p := a.Predict(x)
-		a.Update(x, p, y, sampleW)
-		loss <- util.Logloss(p, y, sampleW)
-	}
-	close(loss)
 }
 
 // Fit fits model with given sample, label and sample weight
